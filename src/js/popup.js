1c68215beea8ec864a180c8eec31b8eadf077a69
Dmitri611 <dimakkk1181@mail.ru>
2021-11-02 14:41:12 +0300

    feat: add popup

---
+++ src/js/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", function (e) {
+  let sortPopup = document.getElementById("sortPopup");
+  if (e.target.id != "popup" && e.target.id != "sortPopup") {
+    sortPopup.style.display = "none";
+  } else if (e.target.id == "popup") {
+    sortPopup.style.display = sortPopup.style.display != "flex" ? "flex" : "none";
+  }
+});
