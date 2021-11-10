ee5f32e5d27c6f866c9668b68140805387bc7ec2
Dmitri611 <dimakkk1181@mail.ru>
2021-11-10 17:32:08 +0300

    feat: delete all files

--- src/modules/variables.js
+++
@@ -1,4 +0,0 @@
-export const btnAdd = document.getElementById('btnAdd');
-export const inputURL = document.getElementById('inputURL');
-export const inputNamePizza = document.getElementById('inputNamePizza');
-export const inputPricePizza = document.getElementById('inputPricePizza');
