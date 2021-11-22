c6b82a3b3ba000bdd9f51a4dc4e4d36a4114414e
Dmitri611 <dimakkk1181@mail.ru>
2021-11-22 14:11:37 +0300

    feat: update pizza component

--- src/components/main/pizza/pizza.jsx
+++ src/components/main/pizza/pizza.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
-import styles from './pizza.module.scss';
-import classNames from 'classnames';
+import React from "react";
+import styles from "./pizza.module.scss";
+import classNames from "classnames";
 
 export default function Pizza(props) {
-    return (
-        <div className={styles.pizza__block}>
+  return (
+    <div className={styles.pizza__block}>
       <div className={styles.pizza__block_image}>
-        <img src={props.image} alt="Pizza"/>
+        <img src={props.image} alt="Pizza" />
       </div>
       <div className={styles.pizza__block_heading}>
         <h4 className={styles.pizza__block_heading_font}>{props.name}</h4>
       </div>
       <div className={styles.pizza__block__ingredients}>
-      <p className={styles.pizza__block_list_item_font}>{props.ingredients}</p>
+        <p className={styles.pizza__block_list_item_font}>
+          {props.ingredients}
+        </p>
       </div>
       <div className={styles.pizza__block__content}>
         <ul className={styles.pizza__block_list}>
-          <li className={classNames(styles.pizza__block_list_item, styles.pizza__block_list_item_active)}>
+          <li
+            className={classNames(
+              styles.pizza__block_list_item,
+              styles.pizza__block_list_item_active
+            )}
+          >
             <span className={styles.pizza__block_list_item_font}>тонкое</span>
           </li>
           <li className={styles.pizza__block_list_item}>
-            <span className={styles.pizza__block_list_item_font}>традиционное</span>
+            <span className={styles.pizza__block_list_item_font}>
+              традиционное
+            </span>
           </li>
         </ul>
         <ul className={styles.pizza__block_list2}>
-          <li className={classNames(styles.pizza__block_list_item,  styles.pizza__block_list_item_active)}>
+          <li
+            className={classNames(
+              styles.pizza__block_list_item,
+              styles.pizza__block_list_item_active
+            )}
+          >
             <span className={styles.pizza__block_list_item_font}>26</span>
           </li>
-          <li
-            className={styles.pizza__block_list_item}>
+          <li className={styles.pizza__block_list_item}>
             <span className={styles.pizza__block_list_item_font}>30</span>
           </li>
           <li className={styles.pizza__block_list_item}>
@@ -53,5 +66,5 @@ export default function Pizza(props) {
         </div>
       </div>
     </div>
-    );
+  );
 }
--- src/components/main/pizzaBlock/pizzaBlock.jsx
+++ src/components/main/pizzaBlock/pizzaBlock.jsx
@@ -12,6 +12,20 @@ export default function PizzaBlock () {
             price='10.99'
             amount='2'
             ></Pizza>
+            <Pizza 
+            image='https://dodopizza-a.akamaihd.net/static/Img/Products/c54928acb21147578a4d8a704cadea11_292x292.jpeg'
+            name='Новая пицца'
+            ingredients='Тесто, Мясо, Соус, Сыр'
+            price='10.99'
+            amount='2'
+            ></Pizza>
+            <Pizza 
+            image='https://dodopizza-a.akamaihd.net/static/Img/Products/c54928acb21147578a4d8a704cadea11_292x292.jpeg'
+            name='Новая пицца'
+            ingredients='Тесто, Мясо, Соус, Сыр'
+            price='10.99'
+            amount='2'
+            ></Pizza>
         </div>
     );
 }
